f58745ee3ce751a15eff90459bd33855bee85d45
danielfayo <daniel123olu@gmail.com>
2023-09-24 20:31:36 +0100

    Added Dialog box to colums

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -5,15 +5,15 @@ import * as Dialog from "@radix-ui/react-dialog";
 import React from "react";
 
 type DialogModalProps = {
-  triggerText: React.ReactElement;
+  trigger: React.ReactElement;
   title: React.ReactElement;
   description?: React.ReactElement
-  button: React.ReactElement
+  button?: React.ReactElement
   content: React.ReactElement
 };
 
 const DialogModal: React.FC<DialogModalProps> = ({
-  triggerText,
+  trigger,
   title,
   description,
   button,
@@ -21,10 +21,10 @@ const DialogModal: React.FC<DialogModalProps> = ({
 }) => {
   return (
     <Dialog.Root>
-      <Dialog.Trigger asChild>{triggerText}</Dialog.Trigger>
+      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
       <Dialog.Portal className="bg-White dark:bg-Dark-Grey data-[state=open]:animate-overlayShow fixed inset-0">
-        <Dialog.Overlay className="bg-Black opacity-50 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] p-4 md:p-8 translate-x-[-50%] translate-y-[-50%] rounded-lg bg-White dark:bg-Dark-Grey shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Overlay className="bg-Black opacity-50 z-20 data-[state=open]:animate-overlayShow fixed inset-0" />
+        <Dialog.Content className="w-full max-w-[38rem] z-20 data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] p-4 md:p-8 translate-x-[-50%] translate-y-[-50%] rounded-lg bg-White dark:bg-Dark-Grey shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <Dialog.Title asChild>{title}</Dialog.Title>
           <Dialog.Description asChild>{description}</Dialog.Description>
           {content}
